03352e85353e7ad6caf8ce679164ba298fe5222f
nikonikpay <nikonikpay@gmail.com>
2022-05-16 19:44:59 +0200

    second day of react

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+
+import './homepage.styles.scss'
+import Directory from "../../components/Directory/Directory";
+
+const HomePage = () => (
+
+    <div className='homepage'>
+
+     <Directory/>
+    </div>
+
+)
+
+
+export default HomePage;
